89065ba0b113c2d209754c4fa630ea38d37ea3c9
Scott Ballantyne <ussballantyne@gmail.com>
2011-10-30 19:48:03 +0800

    trying something else

--- lib/omniauth/strategies/jiepang.rb
+++ lib/omniauth/strategies/jiepang.rb
@@ -6,7 +6,7 @@ module OmniAuth
   module Strategies
     class Jiepang < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site          => 'https://jiepang.com',
+        :site          => 'http://api.jiepang.com',
         :authorize_url => '/oauth/authorize',
         :token_url     => '/oauth/access_token'
       }
